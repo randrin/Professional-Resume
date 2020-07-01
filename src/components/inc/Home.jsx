739e1bc3e3eb0cb98cfd5a97e9c7fe9ed5717606
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-07-02 00:07:53 +0200

    Test CI (4)

--- src/components/inc/Home.jsx
+++ src/components/inc/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, NavBar } from "../../pages";
+import { Header, NavBar } from "../../pages/index";
 import { About, Resume, Blog, Contact, Portfoglio } from "../index";
 export default class Home extends Component {
   render() {
