7bce6a01ea89c26032f907c2dd72cb4ab0679d08
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-07-02 10:25:43 +0200

    Test CI (11)

--- src/components/inc/Home.jsx
+++ src/components/inc/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import NavBar from "../../pages/site/NavBar";
 import { About, Resume, Blog, Contact, Portfoglio } from "../index";
+import { Translation } from "react-i18next";
+import MultiLanguages from "../../pages/inc/MultiLanguages";
 export default class Home extends Component {
   render() {
     return (
@@ -34,7 +35,141 @@ export default class Home extends Component {
           {/* Header End */}
           <div id="main" className="site-main">
             <div className="pt-wrapper">
-              <NavBar />
+              {/* NavBar Start */}
+              <Translation>
+                {(t) => (
+                  <>
+                    <div className="site-nav mobile-menu-hide">
+                      <div className="container-fluid">
+                        <div className="row">
+                          <div className="col-md-6">
+                            <ul id="nav" className="site-main-menu">
+                              <li>
+                                <a
+                                  className="pt-trigger"
+                                  href="#about_me"
+                                  data-animation="58"
+                                  data-goto="1"
+                                >
+                                  {t("ABOUT.TITLE")}
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="pt-trigger"
+                                  href="#resume"
+                                  data-animation="59"
+                                  data-goto="2"
+                                >
+                                  {t("RESUME.TITLE")}
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="pt-trigger"
+                                  href="#portfolio"
+                                  data-animation="60"
+                                  data-goto="3"
+                                >
+                                  {t("PORTFOLIO.TITLE")}
+                                </a>
+                              </li>
+                              <li className="resume-blog">
+                                <a
+                                  className="pt-trigger"
+                                  href="#blog"
+                                  data-animation="61"
+                                  data-goto="4"
+                                >
+                                  {t("BLOG.TITLE")}
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="pt-trigger"
+                                  href="#contact_me"
+                                  data-animation="58"
+                                  data-goto="5"
+                                >
+                                  {t("CONTACT.TITLE")}
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                          <div className="col-md-6 socials-links-multilanguages">
+                            <ul className="social-links">
+                              <li>
+                                <a
+                                  className="tip social-button"
+                                  href="https://github.com/randrin"
+                                  target="_blank"
+                                  title="Github"
+                                >
+                                  <i className="fa fa-github"></i>
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="tip social-button"
+                                  href="https://bitbucket.org/randrin/?visibility=all"
+                                  target="_blank"
+                                  title="Bitbucket"
+                                >
+                                  <i className="fa fa-bitbucket"></i>
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="tip social-button"
+                                  href="https://twitter.com/NzeukangRandrin"
+                                  target="_blank"
+                                  title="Twitter"
+                                >
+                                  <i className="fa fa-twitter"></i>
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="tip social-button"
+                                  href="https://www.facebook.com/decotino"
+                                  target="_blank"
+                                  title="Facebook"
+                                >
+                                  <i className="fa fa-facebook"></i>
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="tip social-button"
+                                  href="#"
+                                  target="_blank"
+                                  title="Google Plus"
+                                >
+                                  <i className="fa fa-google-plus"></i>
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  className="tip social-button"
+                                  href="https://www.youtube.com/channel/UCugdTE_t5-1bGV_-kqaq0Vg"
+                                  target="_blank"
+                                  title="Youtube"
+                                >
+                                  <i className="fa fa-youtube"></i>
+                                </a>
+                              </li>
+                            </ul>
+                            <ul className="site-multilanguages">
+                              <MultiLanguages />
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </Translation>
+              {/* NavBar End */}
             </div>
             <div className="subpages">
               <About />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,2 @@
+export { default as Header } from "../pages/site/Header";
 export { default as NavBar } from "../pages/site/NavBar";
