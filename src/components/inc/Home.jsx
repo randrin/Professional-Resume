3a9bb008ceb3667d0a02229415a724c2916a26da
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-07-02 10:12:02 +0200

    Test CI (9)

--- src/components/inc/Home.jsx
+++ src/components/inc/Home.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
-import { Header, NavBar } from "../../pages";
+import { NavBar } from "../../pages";
 import { About, Resume, Blog, Contact, Portfoglio } from "../index";
 export default class Home extends Component {
   render() {
     return (
       <>
         <div id="page" className="page">
-          <Header />
+          <header id="site_header" className="header banner_water_effect">
+            <div className="my-photo">
+              <img src="../assets/images/Randrin.png" alt="image" />
+              <div className="mask"></div>
+            </div>
+
+            <div className="site-title-block">
+              <a href="https://www.templateshub.net" target="_blank">
+                <h1 className="site-title">Randrin Nzeukang</h1>
+              </a>
+              <h1 className="cd-headline clip is-full-width text-uppercase">
+                <span className="color_white">I am a</span>
+                <span className="cd-words-wrapper color_default">
+                  <b className="is-visible">UI/UX Designer.</b>
+                  <b>Developer.</b>
+                  <b>Freelancer.</b>
+                </span>
+              </h1>
+            </div>
+
+            <a className="menu-toggle mobile-visible">
+              <i className="fa fa-bars"></i>
+            </a>
+          </header>
           <div id="main" className="site-main">
             <div className="pt-wrapper">
               <NavBar />
