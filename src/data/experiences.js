e4591c605e9d631a8a0f8456a6ceda3abb96c88d
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-10-26 23:28:42 +0100

    Compilation Experiences informations

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -38,6 +38,93 @@ export const experiences = {
       tools:
         "Micro services, Spring Boot (Data, Security, MVC), JPA, IntelliJ, Visual Studio Code, JQuery, JavaScript, Angular 7, HTML5/CSS3, Bootstrap, Sass, Jira, Bitbucket, Postman, TortoiseSVN, TortoiseGit, Response Devices.",
     },
+    {
+      id: 2,
+      society: "Accenture",
+      societyDescription:
+        "Accenture est une société multinationale de services professionnels fournissant des services en stratégie, conseil, numérique, technologie et opérations.",
+      jobDescription:
+        "J'ai participé au développement de composants (modules), à l'intégration et à la maintenance d'applications client / serveur pour les sites e-commerce d'Illy Caffè (Illy Italia, Illy Francia, Illy Usa). Les applications sont dédiées à la gestion de l'exposition de la nouvelle marque de café dans le monde, y compris la configuration de chaque page, l'émission de textes, paragraphes, images, produits sur différents modèles de modèles visibles du public.",
+      jobTitle: "Développeur Front End",
+      jobStart: "Avril 2018",
+      jobEnd: "Mars 2019",
+      isCurrentJob: false,
+      responsabilities: [
+        {
+          task:
+            "Développement de modules (composants en HTML5 / CSS3 / JavaScript / JQuery / Foundation Css) avec des pixels créés par l'équipe UX / UI Designer sur la plateforme Zeplin;",
+        },
+        {
+          task:
+            "Intégration des composants logiciels sur AEM afin qu'ils soient facilement configurables par l'utilisateur;",
+        },
+        {
+          task:
+            "Maintenance de la plateforme évolutive pour les demandes de changement des clients et des services web mis à jour;",
+        },
+        {
+          task:
+            "Développement d'approches sur mesure, basées sur les besoins des clients;",
+        },
+        {
+          task:
+            "Participation à la distribution des applications de mise à jour (production et tests) pour l'équipe fonctionnelle;",
+        },
+        {
+          task:
+            "Communication en méthodologie Scrum / Agile sur Trello avec l'équipe projet;",
+        },
+        {
+          task:
+            "Correction de bugs sur l'interface Atlassian Jira tout en respectant les délais;",
+        },
+        {
+          task: "Support immédiat pour le client et ses utilisateurs.",
+        },
+      ],
+      tools:
+        "Visual Studio Code, JQuery, JavaScript, Web Services, Node.js, Gulp, HTML5/CSS3, Bootstrap4, Sass, Foundation Css, AEM (Adobe Experience Manager), Saleforces, Atlassian Jira, Trello, Zeplin, Web Accessibility, Git, GitLab, Bitbucket",
+    },
+    {
+      id: 3,
+      society: "Sky Italie",
+      societyDescription:
+        "Sky est le leader des médias et du divertissement en Europe et fait partie du groupe Comcast. Les membres de l'établissement aident à connecter les clients avec le meilleur des sports, des nouvelles, des arts et leur contenu local et original.",
+      jobDescription:
+        "Développer une application de reporting CRM, dédiée aux alarmes et à la gestion de la télésurveillance d'un bâtiment sécurisé, pointant les phases d'intervention et communiquant avec le service Help Desk. Le Frontend, composé d'interfaces, met en évidence les différents types d'alarmes et résout les problèmes critiques. L'application répertorie les différents cas d'alarmes, d'un centre de données, à l'utilisateur connecté, y compris les privilèges de modification et de visualisation, l'exportation en masse et l'importation de données du format Excel / CSV dans la base de données et vice versa, un rapport statistique des mouvements des utilisateurs de la structure, ainsi que la communication par message au service Help Desk en cas d'urgence grave.",
+      jobTitle: "Développeur Full Stack",
+      jobStart: "Avril 2018",
+      jobEnd: "Mars 2019",
+      isCurrentJob: false,
+      responsabilities: [
+        {
+          task:
+            "Analyse des besoins de développement d'applications et / ou demandes d'évolution;",
+        },
+        {
+          task:
+            "Conception et contribution à la mise en œuvre des spécifications fonctionnelles / techniques et développement de la nouvelle UI (User Interface);",
+        },
+        {
+          task:
+            "Développement de nouvelles fonctions Frontend (avec Angular 7, Angular Material, TypeScript, HTML5 / CSS3, JavaScript, JQuery) et de la logique métier Backend (avec Java / J2ee, Spring Boot / Security / Data / MVC / Batch);",
+        },
+        {
+          task:
+            "Rédiger et exécuter les différents scénarios associés aux tests;",
+        },
+        {
+          task:
+            "Proposition d'amélioration en termes de performances et d'ergonomie;",
+        },
+        {
+          task:
+            "Accompagnement du client et des utilisateurs de l'application.",
+        },
+      ],
+      tools:
+        "Java/J2ee, Spring (Core, Security, Data, MVC, Boot, Batch), JPA, Hibernate, RestFull API, Angular7, Angular Material, PrimeNG, Visual Studio Code, JQuery, Eclipse IDE, JavaScript, HTML5/CSS3, Bootstrap4, Oracle, SVN, GitLab",
+    },
   ],
   EN: [],
   IT: [],
