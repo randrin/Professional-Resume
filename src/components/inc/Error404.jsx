6c0ec148b72e8925edec9b7ae48a057a4240036e
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-03-16 23:42:40 +0100

    first commit error 404

--- src/components/inc/Error404.jsx
+++ src/components/inc/Error404.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from "react";
 import { Header, NavBar, Footer } from "..";
+import { Translation } from "react-i18next";
 
 export default class Error404 extends PureComponent {
   render() {
@@ -11,8 +12,28 @@ export default class Error404 extends PureComponent {
             <NavBar />
           </div>
           <div className="subpages-wrapper subpages">
-            <h1>Erro404 Page</h1>
-            <Footer />
+            <section className="pt-page pt-page-current">
+              <Translation>
+                {(t) => (
+                  <>
+                    <div className="section-title-block">
+                      <h2 className="section-title">{t("ERROR_404.TITLE")}</h2>
+                      <h5 className="section-description">
+                        {t("ERROR_404.SUB_TITLE")}
+                      </h5>
+                    </div>
+                    <div className="container subpages-wrapper subpages">
+                      <div className="row">
+                        <div className="col-md-12">
+                          <h1>Erro404 Page</h1>
+                        </div>
+                      </div>
+                      <Footer />
+                    </div>
+                  </>
+                )}
+              </Translation>
+            </section>
           </div>
         </div>
       </div>
